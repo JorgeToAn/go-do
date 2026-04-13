4c4da84eca47a147a3cad93f7a80fe0d92d85d01
JorgeToAn <jorgetosta.f@gmail.com>
2026-04-12 21:26:50 -0700

    fix typo

--- main.go
+++ main.go
@@ -8,17 +8,17 @@ import (
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Go-Do is a CLI utility for efficient task management.")
-		fmt.Println("Usage:\n  godo [command]")
+		fmt.Println("Usage:\n  go-do [command]")
 		fmt.Println("Available Commands:")
 		fmt.Println()
 		fmt.Println("Flags:")
-		fmt.Println("  --version  version for godo")
+		fmt.Println("  --version  version for go-do")
 		os.Exit(0)
 	}
 
 	args := os.Args[1:]
 
 	if args[0] == "--version" {
-		fmt.Println("godo v0.0.0")
+		fmt.Println("go-do v0.0.0")
 	}
 }
